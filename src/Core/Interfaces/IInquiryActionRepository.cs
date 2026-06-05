786aeeb081b4465bdb29d730edd82b1e92435b70
Danni <33860063+soridev@users.noreply.github.com>
2026-06-06 01:20:58 +0200

    add interface for specific repos

---
+++ src/Core/Interfaces/IInquiryActionRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces;
+
+public interface IInquiryActionRepository
+{
+    Type SupportedType { get; }
+    Task CancelAsync(Inquiry inquiry);
+    Task CompleteAsync(Inquiry inquiry);
+}
---
+++ src/Core/PersonalFinancalInquiryRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Contexts;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core;
+
+public class PersonalFinancalInquiryRepository : IInquiryActionRepository
+{
+    public Type SupportedType => typeof(PersonalFinancalInquiry);
+
+    public Task CancelAsync(Inquiry inquiry)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CompleteAsync(Inquiry inquiry)
+    {
+        throw new NotImplementedException();
+    }
+}
