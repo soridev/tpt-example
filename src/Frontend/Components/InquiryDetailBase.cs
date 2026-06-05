6cab4a07c801e5c620c86c4d96909a0daf47c071
Danni <33860063+soridev@users.noreply.github.com>
2026-06-06 00:59:28 +0200

    add base class for all inquiry detail components

---
+++ src/Frontend/Components/InquiryDetailBase.cs
@@ -0,0 +1,10 @@
+﻿using Core.Contexts;
+using Microsoft.AspNetCore.Components;
+
+namespace Frontend.Components;
+
+public abstract class InquiryDetailBase<T> : ComponentBase where T : Inquiry
+{
+    [Parameter] public required Inquiry Data { get; set; }
+    protected T Inquiry => (T)Data;
+}
